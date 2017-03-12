f7cd05e22ba0d0ae17771bfbc41617a1e77868b8
= <=>
2017-03-12 14:41:51 -0700

    get basic RNN running

--- RNN.py
+++ RNN.py
@@ -7,6 +7,8 @@ from keras.layers import Dense, Activation, Flatten, Dropout, Merge, merge, LSTM
 
 
 N_CLASSES = 13
+P = 0.33
+T = 8
 
 raw_data = np.genfromtxt('data/harmonyData.csv', delimiter=',', dtype=np.int32, skip_header = 1)
 raw_X = raw_data[:,0:8]
@@ -27,8 +29,41 @@ for i in np.arange(N):
   cat_X.append(cat_x)
   cat_Y.append(cat_y)
 
-print(np.shape(cat_X))
-print(np.shape(cat_Y))
+cat_X = np.asarray(cat_X)
+cat_Y = np.asarray(cat_Y)
+
+shuf = np.random.permutation(N)
+
+X = cat_X[shuf, :, :]
+Y = cat_Y[shuf, :, :]
+
+train_N = np.ceil(N*(1-P)).astype("int32")
+
+train_X = X[0:train_N,:,:]
+train_Y = Y[0:train_N,:,:]
+
+test_X = X[train_N:,:,:]
+test_Y = X[train_N:,:,:]
+
+model = Sequential()
+model.add(LSTM(20, return_sequences=True, input_shape=(T, N_CLASSES)))
+model.add(LSTM(N_CLASSES, return_sequences=True))
+#model.add(Activation('softmax'))
+
+sgd = SGD(lr=0.01, momentum=0.1)
+
+model.compile(loss = 'categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.fit(train_X, train_Y, nb_epoch = 10, validation_split=0.1, verbose = 2)
+
+test_res = model.test_on_batch(test_X, test_Y)
+
+print "loss {0}, accuracy {1}".format(test_res[0], test_res[1])
+
+
+
+
+
 
 
 
